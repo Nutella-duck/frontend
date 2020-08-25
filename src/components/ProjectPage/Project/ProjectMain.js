553b0ebb514ddb5a59516788baac1902e8d835b5
haeina <leehaein0164@gmail.com>
2020-08-25 15:20:47 +0900

    not connect to server

--- src/components/ProjectPage/Project/ProjectMain.js
+++ src/components/ProjectPage/Project/ProjectMain.js
@@ -18,42 +18,7 @@ import * as Actions from "../../../store/actions";
      });
      
 
-        const [users, setUsers] = useState(null);
-        const [loading, setLoading] = useState(false);
-        const [error, setError] = useState(null);
       
-        const fetchUsers = async () => {
-          try {
-            // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-            setError(null);
-            setUsers(null); 
-            // loading 상태를 true 로 바꿉니다.
-            setLoading(true);
-            const response = await axios.get(
-              'http://localhost:7000/admin/project'
-            );
-            setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
-             
-           
-        
-          } catch (e) {
-            setError(e);
-          }
-        
-          setLoading(false);
-        };
-      
-        useEffect(() => {
-          fetchUsers();
-        }, []);
-       if (!users) return null;
-        const click = () => {
-          
-          fetchUsers();
-          console.log(users.project_name);
-          dispatch(Actions.addProject(users.project_name,users.description));
-        
-        }
         
     
 
@@ -92,11 +57,11 @@ import * as Actions from "../../../store/actions";
           };
 
           
-          if (!users) return null;
+          
   
           return (
             <>
-            <button onClick={click}>다시 불러오기</button>
+           
             <ProjectListTemplate form={(
               <ProjectForm 
               projectName={projectName}
