35177d480a993e3550e161ac321155b5738d2ef5
leehaeina <leehaein0164@gmail.com>
2020-10-21 04:12:39 +0900

    hpo 추가 기능 구현 (스토어사용하지않고 단순 추가만 구현)

--- src/components/WorkSpace/RunTableComponent.js
+++ src/components/WorkSpace/RunTableComponent.js
@@ -1,17 +1,73 @@
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button,Modal,Col,Container,Row } from 'react-bootstrap';
 
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { RowNodeCache } from 'ag-grid-community';
 
 const App = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [inputs, setInputs] = useState({
+    HPOName: '',
+    description: '',
+  });
+  const { HPOName, description } = inputs;
+  const [apiKey,setApiKey]=useState();
+const getKey=()=>{
+ setApiKey(100);
+}
+const onCreate=()=>{
+  setInputs({
+    HPOName: '',
+    description: '',
+  });
+  console.log("setinputs",inputs);
+  let HPOInfoData = {
+    name: inputs.HPOName,
+    state: inputs.description,
+    runCount: apiKey,
+    created: 35000, computedTime: '1hours',createdBy:'leehaein'
+  };
+  setRowData(rowData.concat(HPOInfoData));
+
+
+  // dispatch(Actions.addProject(projectInfoData));
+
+  // state.push(projectInfoData);
+  
+}
+const onChange = (e) => {
+  const { name, value } = e.target;
+  setInputs({
+    ...inputs,
+    [name]: value,
+  });
+};
+  const handleShow2 = () => {
+    getKey();
+    setShow2(true);
+  };
+
+  const handleClose2 = () => {
+    setShow2(false);
+    setShow(false);
+    onCreate();
+  };
 
   const [rowData, setRowData] = useState([
-    { name: 'hpo1', state: 'running', created: 35000, computedTime: '1hours', runCount: 115,createdBy:'leehaein' },
+    { name: 'hpo1', state: 'running', created: 35000, computedTime: '1hours', runCount: 115,createdBy:'leehaein' },{computedTime: "1hours"
+    ,created: 35000,
+    createdBy: "leehaein",
+    name: "dfwe",
+    runCount: 100,
+    state: "gewg"},
     { name: 'hpo1', state: 'running', created: 35000, computedTime: '1hours', runCount: 115,createdBy:'leehaein'  },{ name: 'hpo1', state: 'running', created: 35000, computedTime: '1hours', runCount: 115,createdBy:'leehaein'  },
   ]);
 
@@ -77,8 +133,84 @@ const columnDef =[
             className="mr-sm-2"
           />
         </Form>
-        <Button>New HPO</Button>
+        <Button onClick={handleShow}>New HPO</Button>
       </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Container>
+            <Col>
+              <Modal.Title>New Project</Modal.Title>
+            </Col>
+            <Col>
+              <p>Create a New Project</p>
+            </Col>
+          </Container>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col>
+                <p>Project Name</p>
+              </Col>
+              <Col>
+                <input
+                  name="HPOName"
+                  onChange={onChange}
+                  value={HPOName}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>Description</p>
+              </Col>
+              <Col>
+                <input
+                  name="description"
+                  onChange={onChange}
+                  value={description}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            CANCEL
+          </Button>
+          <Button variant="primary" onClick={handleShow2}>
+            CREATE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={show2} onHide={handleClose2}>
+        <Modal.Header closeButton>
+          <Container>
+            <Col>
+              <Modal.Title>New Project</Modal.Title>
+            </Col>
+            <Col>
+              <p>New Project Created!</p>
+            </Col>
+          </Container>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Col>
+              <p>Your API Key is</p>
+            </Col>
+            <Col>
+              <p>{apiKey}</p>
+            </Col>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose2}>
+            DONE
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div
         className="ag-theme-alpine"
         style={{ height: '1600px', width: '2000px' }}
