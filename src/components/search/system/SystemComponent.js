30f7d3f069b474f9de2979f0cfd0f5afe277fa3d
leehaeina <leehaein0164@gmail.com>
2021-01-13 06:44:02 +0900

    workspace system 정리, *sections 패널 추가기능 구현 필요

--- src/components/search/system/SystemComponent.js
+++ src/components/search/system/SystemComponent.js
@@ -4,18 +4,23 @@ import SystemComponents from './SystemComponents';
 import { useSelector } from 'react-redux';
 
 const SystemComponent = ({ models, graph, isLoading }) => {
-  const systems = useSelector((state) => state.system.cards);
+  //const systems = useSelector((state) => state.system.cards);
 
   return (
     <div style={{ paddingBottom: '3rem' }}>
-      {systems.map((system, index) => (
+      {/* {systems.map((system, index) => (
         <SystemComponents
           key={index}
           models={models}
           graph={graph}
           isLoading={isLoading}
         ></SystemComponents>
-      ))}
+      ))} */}
+        <SystemComponents
+          models={models}
+          graph={graph}
+          isLoading={isLoading}
+        ></SystemComponents>
     </div>
   );
 };
