0eb88770ef0ba53deeac6c2a4cf4002904afc590
haeina <leehaein0164@gmail.com>
2021-02-04 21:23:18 +0900

    이미지 업로드 후 미리보기설정

--- src/components/LoginPage/SingupPage.js
+++ src/components/LoginPage/SingupPage.js
@@ -3,6 +3,7 @@ import React,{useState} from "react";
 import './SignupPage.css'
 
 const SignupPage = () =>{
+    const [img ,setImage] = useState({file:null,previewURL:""})
     const [user,setUser] = useState({userId:'',password:'',nickname:'',email:null ,location:null,company:null,introduction:null});
     const onClick= ()=>
     {
@@ -19,8 +20,46 @@ const SignupPage = () =>{
     const goToHome=()=>{
         window.location=`/`
     }
+    const handleFileOnChange = (event) => {
+        event.preventDefault();
+        console.log(event)
+        let reader = new FileReader();
+        let file = event.target.files[0];
+        reader.onloadend = () => {
+          setImage({
+            file : file,
+            previewURL : reader.result
+          })
+          console.log(img)
+        }
+        
+        reader.readAsDataURL(file);
+        console.log(reader)
+      }
+    
+      const ImageClick = async () => {
+        const formData = new FormData();
+        formData.append('file', img);
+        console.log(formData);
+        // 서버의 upload API 호출
+       // const res = await axios.post("/api/upload", formData);
+        //console.log(res);
+      }
+      let profile_preview = null;
+    if(img.file !== ''){
+      profile_preview = <img className='profile_preview' src={img.previewURL}></img>
+    }
     return(
         <div className="registerForm">
+            {/* <input type="file" name="file" onChange={null} ref={ref => {
+                this.uploadInput = ref;
+              }}/> */}
+               <input type='file' 
+      accept='image/jpg,image/png,image/jpeg,image/gif' 
+      name='profile_img' 
+      onChange={handleFileOnChange}>
+  </input> {profile_preview}
+
             <div className="input">
                 <div >ID : </div>
                 <input placeholder="username" id="userId"onChange={onChange}></input>
@@ -46,7 +85,7 @@ const SignupPage = () =>{
             </div>
 
         <button onClick={onClick}>sign up</button>
-        <button onClick={goToHome}>홈화면으로</button>
+        <button onClick={ImageClick}>홈화면으로</button>
         </div>
     )
 }
